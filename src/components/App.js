bd647afea433696d19a0006db7830e62040039f2
Ioannis Poulakas <giannis.p@gmail.com>
2020-03-21 14:32:48 +0200

    Add entrypoint

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+/**
+ * @file App component.
+ */
+
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <p>Hello World!</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,19 @@
+/**
+ * @file Main entrypoint.
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/App';
+
+const rootEl = document.getElementById('root');
+ReactDOM.render(<App />, rootEl);
+
+if (module.hot) {
+  module.hot.accept('./components/App', () => {
+    // eslint-disable-next-line global-require
+    const NextApp = require('./components/App').default;
+    ReactDOM.render(<NextApp />, rootEl);
+  });
+}
