3b5fd1fb926ce2850826ff87dc89b2e2f8b3886a
Ioannis Poulakas <giannis.p@gmail.com>
2020-03-22 11:13:24 +0200

    Add custom mui theme

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Helmet from 'react-helmet';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import { blue } from '@material-ui/core/colors';
 import { CssBaseline, Grid } from '@material-ui/core';
 
 import Navigation from './Navigation';
@@ -12,20 +14,28 @@ import About from '../pages/About';
 import Home from '../pages/Home';
 import NotFound from '../pages/NotFound';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: blue,
+  },
+});
+
 function App() {
   return (
     <Router>
       <CssBaseline />
       <Helmet titleTemplate="%s - React Boilerplate" />
-      <Navigation />
+      <ThemeProvider theme={theme}>
+        <Navigation />
 
-      <Grid container>
-        <Switch>
-          <Route exact path="/about" component={About} />
-          <Route exact path="/" component={Home} />
-          <Route component={NotFound} />
-        </Switch>
-      </Grid>
+        <Grid container>
+          <Switch>
+            <Route exact path="/about" component={About} />
+            <Route exact path="/" component={Home} />
+            <Route component={NotFound} />
+          </Switch>
+        </Grid>
+      </ThemeProvider>
     </Router>
   );
 }
