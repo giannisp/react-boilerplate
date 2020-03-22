7ec6f1b7ea8a71b88422f72c3f590e89ab9d26be
Ioannis Poulakas <giannis.p@gmail.com>
2020-03-22 10:34:12 +0200

    Use mui navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,20 +3,29 @@
  */
 
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
+import { AppBar, Tabs, Tab } from '@material-ui/core';
+
+const menu = [
+  { title: 'Home', url: '/' },
+  { title: 'About', url: '/about' },
+];
 
 function Navigation() {
+  const history = useHistory();
+
+  function onTabChange(event, value) {
+    history.push(value);
+  }
+
   return (
-    <div>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-      </ul>
-    </div>
+    <AppBar position="static">
+      <Tabs onChange={onTabChange} aria-label="Menu">
+        {menu.map(item => (
+          <Tab label={item.title} value={item.url} />
+        ))}
+      </Tabs>
+    </AppBar>
   );
 }
 
