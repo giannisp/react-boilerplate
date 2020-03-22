ef350c493990585bd1e648fd6e9d3e5877acf3ab
Ioannis Poulakas <giannis.p@gmail.com>
2020-03-22 11:13:38 +0200

    Apply custom style for tabs indicator

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,15 +5,19 @@
 import React from 'react';
 import { useLocation, useHistory } from 'react-router-dom';
 import { AppBar, Tabs, Tab } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
 const menu = [
   { title: 'Home', url: '/' },
   { title: 'About', url: '/about' },
 ];
 
+const useStyles = makeStyles({ indicator: { backgroundColor: 'white' } });
+
 function Navigation() {
   const location = useLocation();
   const history = useHistory();
+  const classes = useStyles();
 
   function onTabChange(event, value) {
     history.push(value);
@@ -21,7 +25,12 @@ function Navigation() {
 
   return (
     <AppBar position="static">
-      <Tabs value={location.pathname} onChange={onTabChange} aria-label="Menu">
+      <Tabs
+        value={location.pathname}
+        onChange={onTabChange}
+        classes={{ indicator: classes.indicator }}
+        aria-label="Menu"
+      >
         {menu.map(item => (
           <Tab key={item.url} label={item.title} value={item.url} />
         ))}
